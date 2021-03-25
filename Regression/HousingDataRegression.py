4a498ae1a9b96aa0d31aead13f52b2243d45dcb7
Matt <cattmollyer@gmail.com>
2021-03-25 13:40:00 -0400

    Finishing up regression. Starts gradient ascent

--- Regression/HousingDataRegression.py
+++ Regression/HousingDataRegression.py
@@ -1,20 +1,33 @@
 import pandas as pd
-from RegressionMultivariate import *
+from Regression import *
+import numpy as np
 
 
 
+data = pd.read_csv('Data/Housing/training.csv')
 
-data = pd.read_csv('Data/Demo/demo_train.csv')
+# X = data['X']
+# Y = data['Y']
+data = normalize(data)
+X1 = data['LandSF']
+X2 = data['TotalFinishedArea']
+Y = data['TotalAppraisedValue']
 
-X = data['X']
-Y = data['Y']
+X = np.column_stack((X1, X2))
 
 
+# results = regression(X, Y, limit = 8)
 
-results = regression(X, Y, limit = 8)
+# best_result = lowest_mse(results)
+# print(best_result)
 
-best_result = lowest_mse(results)
-print(best_result)
+W = [ 1.07878098e-03,  8.11974341e-01, -5.87133491e-01,  6.59019988e-01,
+        1.12655324e+00,  7.86595013e-01,  4.44828851e-02, -8.46061920e-01,
+       -1.75772269e+00,  3.19034542e-01,  4.81801337e-01, -5.02154833e-01,
+       -7.00213023e-01, -4.49279353e-01, -6.39765155e-02,  3.30600966e-01,
+        7.02834290e-01]
 
-graph(X, Y, best_result['Weights'])
+
+Y = np.column_stack((Y,Y))
+graph(X, Y, W)
 
--- Regression/Real_Estate_Data_Clean.py
+++ Regression/Real_Estate_Data_Clean.py
@@ -1,19 +1,29 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-data = pd.read_csv('Real_Estate_Sales_730_Days.csv')
+
+def shuffle_divide_export(df, training_percent = 0.8, validation_percent = 0.1, shuffle = True, path = 'Data/Housing/'):
+  if(shuffle):
+    df = df.sample(frac=1)
+  training, validation, test = np.split(df, [int(training_percent*len(df)), int((1-validation_percent)*len(df))])
+  training.to_csv(path+'training.csv', index = False)
+  validation.to_csv(path+'validation.csv', index = False)
+  test.to_csv(path+'test.csv', index = False)
+
+
+
+
+data = pd.read_csv('Data/Housing/Real_Estate_Sales_730_Days.csv')
 
 data = data[['LandSF','TotalFinishedArea','TotalAppraisedValue']]
-
-  
-
+data = data.dropna()
 data = data[data > 0]
 data = data[data['TotalAppraisedValue'] < 5000000]
 data = data[data['LandSF'] < 350000]
 
 
-
-data.to_csv('real_estate.csv', index=False)  
+shuffle_divide_export(data)
 
 # plt.scatter(data['TotalFinishedArea'],data['TotalAppraisedValue'])
 # plt.scatter(data['LandSF'], data['TotalAppraisedValue'])
